e9971951bdb00adf831dfe1aeee6c655c4eea8ef
kronotiger <104576242+kronotiger@users.noreply.github.com>
2022-05-22 19:20:35 +0200

    Update numIdContext.js

--- src/pages/context/numIdContext.js
+++ src/pages/context/numIdContext.js
@@ -1,5 +1,10 @@
 import {createContext} from "react";
 
+/**
+ * Contexto generado que contendrá un número, con este seleccionaremos que archivo queremos de la playlist y lo asignameros al contexto song siempre que cambiemos
+ * @type {React.Context<null>}
+ */
+
 const numIdContext = createContext(null);
 
 export default numIdContext;
